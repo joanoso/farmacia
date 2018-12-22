1e46ee3e1b1bab5a59ce1a11b246d3f528ea9129
joano <carmodo@est.frba.utn.edu.ar>
2018-12-21 21:10:08 -0300

    elimino appold

--- webapp/appOld/common/model/Student.ts
+++
@@ -1,5 +0,0 @@
-export interface Student {
-    id: number,
-    name: string,
-    passport_number: number,
-}
--- webapp/appOld/common/model/System.ts
+++
@@ -1,6 +0,0 @@
-export interface System {
-    version: string
-    serverTime: string
-    serverIP: string
-    properties: { [key: string]: string; }
-}
